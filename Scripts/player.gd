dbe929cc785b48fe4a85c8825b7b89fbaaaa37bc
MustafaBioS <mustaafahany@gmail.com>
2026-01-31 14:18:51 +0800

    ADD - Top Down Movement

---
+++ Scripts/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	var input_vector = Vector2.ZERO
+	
+	input_vector.x = Input.get_axis("a", "d")
+	input_vector.y = Input.get_axis("w", "s")
+	
+	input_vector = input_vector.normalized()
+	
+	velocity = input_vector * SPEED
+	
+	move_and_slide()
